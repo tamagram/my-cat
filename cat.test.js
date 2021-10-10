be05cff8b8e23b3c3e113718ddc0970b2555a204
tamagram <tamagram64@gmail.com>
2021-10-10 12:29:02 +0900

    :white_check_mark: テストを追加

--- cat.test.js
+++ cat.test.js
@@ -1,12 +1,31 @@
 const cat = require("./cat");
 
-test("hoge.json内のテキスト取得", () => {
-  want = `[
+describe("test get_text_content", () => {
+  test("hoge.json内のテキスト取得", () => {
+    want = `[
   {
     "hoge": "fuga"
   }
 ]`;
-  path = "./hoge.json";
-  got = cat.get_text_content(path);
-  expect(got).toBe(want);
+    path = "./hoge.json";
+    got = cat.get_text_content(path);
+    expect(got).toBe(want);
+  });
+});
+
+describe("test attach_text_indent", () => {
+  test("bucketに当たる部分に制御文字を追加", () => {
+    want = [
+      "\x1B[33m[\x1B[0m",
+      "\x1B[35m{\x1B[0m",
+      "\x1B[36m<\x1B[0m",
+      "\x1B[36m>\x1B[0m",
+      "\x1B[35m}\x1B[0m",
+      "\x1B[33m]\x1B[0m",
+    ];
+    text = "[{<>}]";
+    got = cat.text_coloring(text);
+    // console.log(got);
+    expect(got).toStrictEqual(want);
+  });
 });
