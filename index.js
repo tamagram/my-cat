6a9704ec24c52fe1f938921c0c21a3dfdad4b767
tamagram <tamagram64@gmail.com>
2021-10-10 12:35:41 +0900

    :sparkles: bracketに色付け

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #! /usr/bin/env node
-console.log("my-cat!")
+const cat = require("./cat");
+
+const args = process.argv.slice(2);
+const path = args[0];
+if (path) {
+  text = cat.get_text_content(path);
+  cat.text_coloring(text).map((output) => {
+    process.stdout.write(output);
+  });
+  console.log();
+} else {
+  console.error("Error: Please specify a path.");
+}
