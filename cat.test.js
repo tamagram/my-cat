baae5238d0f76680c0181b875d40e131cd2f3393
tamagram <tamagram64@gmail.com>
2021-10-10 14:06:56 +0900

    :adhesive_bandage: リテラル形式だとなんかきもいので変える

--- cat.test.js
+++ cat.test.js
@@ -2,11 +2,7 @@ const cat = require("./cat");
 
 describe("test get_text_content", () => {
   test("hoge.json内のテキスト取得", () => {
-    want = `[
-  {
-    "hoge": "fuga"
-  }
-]`;
+    want = '[{"hoge": "fuga"}]';
     path = "./hoge.json";
     got = cat.get_text_content(path);
     expect(got).toBe(want);
