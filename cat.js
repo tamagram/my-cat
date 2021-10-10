45827b2dd7201ad20048f9eed31e871ee216b536
tamagram <tamagram64@gmail.com>
2021-10-10 12:30:17 +0900

    :ballot_box_with_check: テストをパス

--- cat.js
+++ cat.js
@@ -13,10 +13,14 @@ const COLOR = {
   reset: "\u001b[0m",
 };
 
-const BUCKET = {
+const BRACKET = {
   square: {
     left: "[",
     right: "]",
+  },
+  curly: {
+    left: "{",
+    right: "}",
   },
   round: {
     left: "(",
@@ -28,7 +32,6 @@ const BUCKET = {
   },
 };
 
-// TODO: ファイルの中身（テキスト）を取得する
 exports.get_text_content = (path) => {
   let text = "";
   try {
@@ -41,6 +44,50 @@ exports.get_text_content = (path) => {
 };
 
 // TODO: テキストに色を付ける
-exports.text_coloring = (color, text) => {
-  return;
+exports.text_coloring = (text) => {
+  let color = COLOR.yellow;
+  let outputs = [];
+  for (let i = 0; i < text.length; ++i) {
+    cur_char = text[i];
+    if (
+      cur_char == BRACKET.square.left ||
+      cur_char == BRACKET.curly.left ||
+      cur_char == BRACKET.round.left ||
+      cur_char == BRACKET.angle.left
+    ) {
+      outputs.push(color + cur_char + COLOR.reset);
+      switch (color) {
+        case COLOR.yellow:
+          color = COLOR.magenta;
+          break;
+        case COLOR.magenta:
+          color = COLOR.cyan;
+          break;
+        case COLOR.cyan:
+          color = COLOR.yellow;
+          break;
+      }
+    } else if (
+      cur_char == BRACKET.square.right ||
+      cur_char == BRACKET.curly.right ||
+      cur_char == BRACKET.round.right ||
+      cur_char == BRACKET.angle.right
+    ) {
+      switch (color) {
+        case COLOR.yellow:
+          color = COLOR.cyan;
+          break;
+        case COLOR.magenta:
+          color = COLOR.yellow;
+          break;
+        case COLOR.cyan:
+          color = COLOR.magenta;
+          break;
+      }
+      outputs.push(color + cur_char + COLOR.reset);
+    } else {
+      outputs.push(cur_char);
+    }
+  }
+  return outputs;
 };
